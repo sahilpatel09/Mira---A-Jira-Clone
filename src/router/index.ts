c6f3cc2ee9aa93709b5e02937925251bf7cee07d
Sahil <smppatel999@gmail.com>
2022-03-12 23:14:48 +0530

    Add route dashboard

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,11 +10,14 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/signup',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Signup.vue')
-  }
+  },
+  {
+    path: '/dash',
+    name: 'Dashboard',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Dash.vue')
+  },
+
 ]
 
 const router = createRouter({
