8068ce581892a152649bd17e1eb518f9743b6b29
Sahil <smppatel999@gmail.com>
2022-03-12 23:16:50 +0530

    icon component

---
+++ src/views/Dash.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="">
+    <aside class="block z-50 fixed top-0 left-0 overflow-hidden h-screen w-20 bg-asidecolor transition duration-150">
+      <div class="pt-5 flex flex-col justify-center items-center">
+        <img src="../assets/logo-white.png" alt="" class="w-12">
+        <img src="../assets/search.png" alt="" class="mt-9 w-12">
+        <img src="../assets/plus.png" alt="" class="w-12 ml-2">
+      </div>
+    </aside>
+    <div class="z-49 w-64 fixed absolute top-0 ml-20 bg-gray-200 h-screen px-5 pt-6">
+
+      <div class="flex flex-row items-center gap-2">
+        <DashIcon />
+        <div>
+          <h2 class="font-semibold text-lg">Mira 2.0</h2>
+          <p class="text-xs">Software Project</p>
+        </div>
+      </div>
+
+<!--      <div class="">-->
+<!--        <buton>-->
+<!--          <i></i>-->
+<!--        </buton>-->
+<!--      </div>-->
+
+    </div>
+
+    <div class=" top-0 right-0 ml-84 bg-white ml-80 px-5 pt-6 px-14">
+      <h1 class="text-lg text-gray-500 font-semibold antialiased">Projects / Mira 2.0 / Kanban</h1>
+      <div>
+
+      </div>
+    </div>
+
+
+  </div>
+</template>
+<script>
+import DashIcon from "@/components/dashboard/DashIcon";
+export default {
+  components: {DashIcon}
+}
+</script>
