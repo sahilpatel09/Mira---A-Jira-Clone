922f6bc91a46962d02a6a95442de524867236bdc
Sahil <smppatel999@gmail.com>
2022-03-12 23:15:22 +0530

    Add custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'logo': '#2684FF',
+        'logofill':'#0052CC',
+        'asidecolor': 'rgb(7,71,166)',
+      },
+      fontFamily: {
+        'display': ['-apple-system,BlinkMacSystemFont,"Segoe UI","Roboto","Oxygen","Ubuntu","Fira Sans","Droid Sans","Helvetica Neue",sans-serif',],
+      }
+    },
   },
   plugins: [],
 }
