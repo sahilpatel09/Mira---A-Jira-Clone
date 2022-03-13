f10c8a8fe91811340e86df558a2ec84a1784d811
Sahil <smppatel999@gmail.com>
2022-03-13 14:44:09 +0530

    Add issuebox template layout

---
+++ src/components/dashboard/IssueBox.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="w-full bg-gray-100 py-2">
+    <h2 class="antialiased text-gray-900 font-semibold py-1 pl-3">BACKLOG 2.0</h2>
+    <IssueCard/>
+  </div>
+</template>
+
+<script>
+import IssueCard from "@/components/dashboard/IssueCard";
+
+export default {
+  name: "IssueBox",
+  components: {
+    IssueCard
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
