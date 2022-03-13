98daef22b81db4b6c5922ee97db404ec2b818918
Sahil <smppatel999@gmail.com>
2022-03-13 14:34:56 +0530

    Add kanban board header, Componentization.

---
+++ src/components/dashboard/BoradHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1 class="text-lg text-gray-500 font-semibold antialiased">Projects / Mira 2.0 / Kanban</h1>
+  <div class="flex flex-row justify-between items-center">
+    <h2 class="text-2xl antialiased text-black font-semibold">Kanban board</h2>
+    <div class=" bg-gray-100 flex flex-row items-center gap-2 justify-center shadow-md py-2 px-4 my-2">
+      <img class="w-6" src="https://sahilpatel09.github.io/github.svg" alt="">
+      <h4 class="text-base text-gray-500 text-center font-semibold">Github</h4>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BoradHeader"
+}
+</script>
