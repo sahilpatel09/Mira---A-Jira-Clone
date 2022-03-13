6a74f7eea3ab7763fc3cdb56508fbc07a146929c
Sahil <smppatel999@gmail.com>
2022-03-13 15:53:33 +0530

    Add props to comp and style fixes.

--- src/components/dashboard/IssueBox.vue
+++ src/components/dashboard/IssueBox.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full bg-gray-100 py-2">
-    <h2 class="antialiased text-gray-900 font-semibold py-1 pl-3">BACKLOG 2.0</h2>
+    <h2 class="antialiased text-gray-700 font-medium   text-sm py-1 pl-3">{{ title.toUpperCase() }}</h2>
     <IssueCard/>
   </div>
 </template>
@@ -12,6 +12,9 @@ export default {
   name: "IssueBox",
   components: {
     IssueCard
+  },
+  props: {
+    title: String,
   }
 
 }
